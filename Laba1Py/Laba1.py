1681be2beae089c437088b51f779c8214efca672
Stanislav Vdovychenko <89263211+xnquaint@users.noreply.github.com>
2021-09-19 22:41:30 +0300

    Add files via upload

---
+++ Laba1Py/Laba1.py
@@ -0,0 +1,14 @@
+import sys
+
+# Визначити, скільки повних годин(h) і повних хвилин(m)
+# пройшло з початку доби до k-ої її секунди
+
+print("Input the number of seconds")
+seconds = int(input())
+if seconds > 86400:
+    print("There are only 86400 seconds in a single day. Please try again.")
+    sys.exit()
+hours = seconds // 3600
+minutes = int(seconds % 3600 / 60)
+print(f"The number of hours is {hours}, the number of minutes is {minutes}")
+
