d2dd141c06e77e768a410e2f19c3ba08699cc5bc
Stanislav Vdovychenko <stasfirmino@gmail.com>
2021-09-22 15:03:13 +0300

    added operator "or"

--- Laba1Py/Laba1.py
+++ Laba1Py/Laba1.py
@@ -5,36 +5,7 @@ import sys
 
 print("Input the number of seconds")
 seconds = int(input())
-if seconds
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-/*
-Визначити, скільки повних годин(h) і повних хвилин(m)
-пройшло з початку доби до k-ої її секунди
-*/
-
-
-int main()
-{
-	int s, h, m; // вводимо змінні (секунди, години, хвилини)
-	cout << "Enter the number of seconds" << endl;
-	cin >> s;
-	if (s > 86400 || s < 0)
-	{
-		cout << "There are only 86400 seconds in a single day" << endl;
-		return 0;
-	}
-	h = s / 3600;   // ділимо секунди на їх к-сть в одній годині
-	m = s % 3600 / 60;  // залишок секунд ділимо на їх к-сть в одній хвилині
-	cout << "The number of hours is " << h << endl;
-	cout << "The number of minutes is " << m << endl;
-	return 0;
-
-}
-
-	> or seconds < 0:
+if seconds > or seconds < 0:
     print("There are only 86400 seconds in a single day. Please try again.")
     sys.exit()
 hours = seconds // 3600
