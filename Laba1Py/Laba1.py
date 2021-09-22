82ef1b524e0ad8f76b244a9ecaf13ad3f1814813
Stanislav Vdovychenko <stasfirmino@gmail.com>
2021-09-22 15:06:19 +0300

    added operator "or"

--- Laba1Py/Laba1.py
+++ Laba1Py/Laba1.py
@@ -2,13 +2,14 @@ import sys
 
 # Визначити, скільки повних годин(h) і повних хвилин(m)
 # пройшло з початку доби до k-ої її секунди
-
+SecDay = 86400
+SecMinute = 60
 print("Input the number of seconds")
 seconds = int(input())
-if seconds > 86400  or seconds < 0:
+if seconds > SecDay  or seconds < 0:
     print("There are only 86400 seconds in a single day. Please try again.")
     sys.exit()
-hours = seconds // 3600
-minutes = int(seconds % 3600 / 60)
+hours = seconds // SecDay
+minutes = int(seconds % SecDay / SecMinute)
 print(f"The number of hours is {hours}, the number of minutes is {minutes}")
 
