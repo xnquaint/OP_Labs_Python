bfe0194581e54e82aaf3aab2a8dd85a5ac2dc352
Stanislav Vdovychenko <stasfirmino@gmail.com>
2021-09-22 15:25:39 +0300

    added variables for seconds in a day, seconds in a minute, seconds in an hour

--- Laba1Py/Laba1.py
+++ Laba1Py/Laba1.py
@@ -3,13 +3,15 @@ import sys
 # Визначити, скільки повних годин(h) і повних хвилин(m)
 # пройшло з початку доби до k-ої її секунди
 SecDay = 86400
+SecHour = 3600
 SecMinute = 60
+
 print("Input the number of seconds")
 seconds = int(input())
 if seconds > SecDay  or seconds < 0:
     print("There are only 86400 seconds in a single day. Please try again.")
     sys.exit()
-hours = seconds // SecDay
-minutes = int(seconds % SecDay / SecMinute)
+hours = seconds // SecHour
+minutes = int(seconds % SecHour / SecMinute)
 print(f"The number of hours is {hours}, the number of minutes is {minutes}")
 
